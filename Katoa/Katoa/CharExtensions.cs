6b0f989caffabbf0eb08baa44fc4805aeaa5995e
Jonathan <vanillajonathan@users.noreply.github.com>
2022-07-11 16:40:33 +0200

    Use expression bodied function

--- Katoa/Katoa/CharExtensions.cs
+++ Katoa/Katoa/CharExtensions.cs
@@ -2,20 +2,14 @@
 {
     public static class CharExtensions
     {
-        public static bool IsLower(this char c)
-        {
-            return char.IsLower(c);
-        }
+        public static bool IsLower(this char c) => char.IsLower(c);
 
-        public static bool IsUpper(this char c)
-        {
-            return char.IsUpper(c);
-        }
+        public static bool IsUpper(this char c) => char.IsUpper(c);
 
         public static char SwapCase(this char c)
         {
             if (c.IsLower()) return char.ToUpper(c);
-            if(c.IsUpper()) return char.ToLower(c);
+            if (c.IsUpper()) return char.ToLower(c);
             return c;
         }
 
